3ad2d078abdf8323dbe3b1a179010e9408f09c3f
IIRuuy <165058489+IIRuuy@users.noreply.github.com>
2024-05-10 11:42:25 +0700

    Add files via upload

---
+++ WindowsFormsApp1/Core/Parser/ErrorListener.cs
@@ -0,0 +1,13 @@
+﻿using Antlr4.Runtime;
+using System.IO;
+
+namespace WindowsFormsApp1.Core.Parser
+{
+    internal class ErrorListener : BaseErrorListener
+    {
+        public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            throw new RequireAnotherTokenException(charPositionInLine);
+        }
+    }
+}
---
+++ WindowsFormsApp1/Core/Parser/LexerErrorListener.cs
@@ -0,0 +1,25 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Core.Parser
+{
+    internal class LexerErrorListener : IAntlrErrorListener<int>
+    {
+        public void SyntaxError(
+                TextWriter output,
+                IRecognizer recognizer,
+                int offendingSymbol,
+                int line,
+                int charPositionInLine,
+                string msg, 
+                RecognitionException e
+            ) {
+            throw new RequireAnotherTokenException(charPositionInLine);
+        }
+    }
+}
